25f3f514d4d49dc095b6ca20c8f8e4eeacc21cd4
Nikunj Kadu <134802492+nk112233@users.noreply.github.com>
2023-11-06 10:47:13 +0530

    Create assgn7.java

---
+++ assgn7.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class assgn7 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(2);
+        list1.add(4);
+        list1.add(6);
+        list1.add(8);
+        list1.add(10);
+        System.out.println(list1.size());
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(3);
+        list2.add(5);
+        list2.add(7);
+        list2.add(9);
+        System.out.println(list2.size());
+
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.add(2);
+        list3.add(3);
+        list3.add(5);
+        list3.add(7);
+        list3.add(11);
+        System.out.println(list3.size());
+
+
+
+
+
+    }
+}
